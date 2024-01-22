c09cbd6307ff19db6e2f13d539f2635b5a1ea65c
Roman <asdwka3@gmail.com>
2024-01-22 22:40:18 +0100

    added opened class for header

--- src/js/script.js
+++ src/js/script.js
@@ -4,17 +4,15 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-  
     slidesPerView: 3,
     spaceBetween: 25,
     freeMode: true,
-
-  
     // Navigation arrows
     navigation: {
       nextEl: '.slider__btn-next',
       prevEl: '.slider__btn-prev',
     },
-  
 
-  });
+});
+
+const menuButton = document.querySelector('.hamburger');
