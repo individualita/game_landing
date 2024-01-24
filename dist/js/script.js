8d85ca8a41e491604b3d1c49a48d05ea37770af6
Roman <asdwka3@gmail.com>
2024-01-24 23:23:56 +0100

    JS scrollToSection; toggleMenu

--- dist/js/script.js
+++ dist/js/script.js
@@ -16,15 +16,56 @@ const swiper = new Swiper('.swiper', {
 });
 
 
+
+
 const classes = { //равно строке opened. 
   opened: 'opened'
 }
 
+const menuButton = document.querySelector('.hamburger');
+const header = document.querySelector('.header');
+const navLink = document.querySelectorAll('.nav__link');
+
+
 
 
 const toggleMenu = () => {
+  header.classList.toggle(classes.opened)
+};
 
-}
+/*const scrollToSection = (e) => { 
+  e.preventDefault(); //отменяем действие браузера при клике чтобы не скакал. 
+  const href = e.currentTarget.getAttribute('href'); //Получаем атрибут 'href' текущего элемента, на который кликнули.
+  if(!href || !href.startsWith('#')) return; // Проверяем, существует ли атрибут 'href' и начинается ли он с решетки (#).startsWith применяется к строке. 
 
-const menuButton = document.querySelector('.hamburger');
+  //href - "#editions"
+  const section = href.slice(1); //  // Вырезаем решетку из значения 'href', получаем строку с именем секции (например, "#editions" станет "editions").
+
+  const top = document.getElementById(section)?.offsetTop; //Если секция найдена, получаем её вертикальное смещение относительно начала документа.
+
+  window.scrollTo({ top, behavior: "smooth"}); // Прокручиваем страницу до верхней части указанной секции с плавным эффектом.
+
+};
+
+*/
+
+const scrollToSection = (e) => {
+  e.preventDefault();
+
+  const href = e.currentTarget.getAttribute('href');
+  if (!href || !href.startsWith('#')) return;
+
+  const section = document.querySelector(href);
+  if (!section) return;
+
+  section.scrollIntoView({ behavior: 'smooth'});
+
+
+};
+
+
 menuButton.addEventListener('click', toggleMenu);
+
+navLink.forEach(link => { //это массив, на каждый элемент навешываем событие. 
+  link.addEventListener('click', scrollToSection)
+}); 
